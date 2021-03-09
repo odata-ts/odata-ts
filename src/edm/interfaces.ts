2c449faaef30086fe6868ad5f7046be6b3e9e915
Christof Sprenger <chrispre@microsoft.com>
2021-03-09 07:14:40 -0800

    cleanup

--- src/edm/interfaces.ts
+++ src/edm/interfaces.ts
@@ -1,26 +1,7 @@
 export namespace edm {
-  export type IEdmType = IEdmPrimitiveType | IEdmEntityType | IEdmComplexType | IEdmCollectionType | IEdmEnumType;
-
-  export type EdmTypeKind = "entity" | "complex" | "primitive" | "collection" | "enum";
-
   export type IEdmSchemaElement = IEdmEntityType | IEdmComplexType | IEdmEnumType;
 
   export type EdmElementKind = "entity" | "complex" | "enum";
-
-  export type IEdmPrimitiveType = {
-    readonly typeKind: "primitive";
-    readonly name: string;
-  };
-
-  export type IEdmCollectionType = {
-    readonly typeKind: "collection";
-    readonly elementType: IEdmType;
-  };
-
-  export type IEdmStructuralProperty = {
-    readonly name: string;
-    readonly type: IEdmType;
-  };
 
   export type IEdmEntityType = {
     readonly typeKind: "entity";
@@ -38,6 +19,11 @@ export namespace edm {
     readonly properties: readonly IEdmStructuralProperty[];
   };
 
+  export type IEdmStructuralProperty = {
+    readonly name: string;
+    readonly type: IEdmType;
+  };
+
   export type IEdmEnumType = {
     readonly typeKind: "enum";
     readonly elementKind: "enum";
@@ -50,6 +36,24 @@ export namespace edm {
     readonly name: string;
     readonly value: number | string | undefined;
   };
+
+  // ---------------------------------
+
+  export type IEdmType = IEdmPrimitiveType | IEdmEntityType | IEdmComplexType | IEdmCollectionType | IEdmEnumType;
+
+  export type EdmTypeKind = "entity" | "complex" | "primitive" | "collection" | "enum";
+
+  export type IEdmPrimitiveType = {
+    readonly typeKind: "primitive";
+    readonly name: string;
+  };
+
+  export type IEdmCollectionType = {
+    readonly typeKind: "collection";
+    readonly elementType: IEdmType;
+  };
+
+  // ---------------------------------
 
   export type IEdmSchema = {
     readonly alias: string;
